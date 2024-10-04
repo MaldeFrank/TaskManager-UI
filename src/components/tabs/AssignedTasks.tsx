73ee612ff77758f4683a44c0097629b98bfb3a1b
MaldeFrank <maldefrankmidtgaard@gmail.com>
2024-10-04 21:33:05 +0200

    Updated filteredtask function

--- src/components/tabs/AssignedTasks.tsx
+++ src/components/tabs/AssignedTasks.tsx
@@ -27,8 +27,9 @@ function AssignedTasks({
 
   const today: Date = new Date();// Todays date
   const nextWeek: Date = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000); //A week from today
-  const formattedTodayDate = new Date(`${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`);
+  const lastWeek: Date = new Date(today.getTime() - 7 * 24 * 60 * 60 * 1000); //A week back
   const formattedNextWeekDate = new Date(`${nextWeek.getFullYear()}/${nextWeek.getMonth() + 1}/${nextWeek.getDate()}`);
+  const formattedLastWeek = new Date(`${lastWeek.getFullYear()}/${lastWeek.getMonth() + 1}/${lastWeek.getDate()}`);
 
   const {
     data: profilesData,
@@ -39,10 +40,9 @@ function AssignedTasks({
   //Function to filter tasks based on the date
   const filteredTasks = assignedTasks.filter(task => {
     const taskDateTime = new Date(task.dateTime);
-    const formattedTodayDateTime = new Date(formattedTodayDate); // Convert to datetime object
-    const formattedNextWeekDateTime = new Date(formattedNextWeekDate); // Convert to datetime object
-  
-    return taskDateTime >= formattedTodayDateTime && taskDateTime <= formattedNextWeekDateTime;
+    const formattedNextWeekDateTime = new Date(formattedNextWeekDate); 
+    const formattedLastWeekDateTime = new Date(formattedLastWeek);
+    return  taskDateTime>=formattedLastWeekDateTime&&taskDateTime <= formattedNextWeekDateTime;
   });
 
   //Function to switch state to done and back
