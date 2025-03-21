2f5235f07f6df54a3ad7d2503619219b6362e537
MaldeFrank <maldefrankmidtgaard@gmail.com>
2025-03-21 18:15:48 +0100

    Frontpage

--- src/pages/LogInScreen.tsx
+++ src/pages/LogInScreen.tsx
@@ -1,5 +1,6 @@
 import "../styles/LoginScreen.css";
 import Pligtlisten from "../assets/frontpage/Pligtlisten_logo.png";
+import ExpandableListItem from "../components/ExpandableListItem";
 interface props{
 
 }
@@ -8,12 +9,11 @@ function LoginScreen({
 
 }:props){
 
-
     return(
     <>
     <div className="loginScreen">
     <img src={Pligtlisten} alt="Pligtlisten logo" className="logo_1" />
-    <div id="signInDiv"></div>
+    <div className="signInDiv" id="signInDiv"></div>
     </div>
     </>)
 }
